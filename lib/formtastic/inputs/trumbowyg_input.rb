e8ce3c88d85306765483f7b3410967c8a713dfae
Juan Manuel Carrera <juanma@crowtec.co>
2020-06-09 10:41:26 +0200

    append classnames

--- lib/formtastic/inputs/trumbowyg_input.rb
+++ lib/formtastic/inputs/trumbowyg_input.rb
@@ -4,7 +4,7 @@ module Formtastic
       def to_html
         input_wrapping do
           label_html <<
-          builder.text_area( method, input_html_options.merge( class: 'trumbowyg-textarea' ) )
+          builder.text_area( method, input_html_options.merge( class: 'trumbowyg-textarea' ) { |key, oldval, newval| newval + " " + oldval } )
         end
       end
     end
